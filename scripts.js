6d2335f33b1ca6115e0a2653ba812c9dbf40f1db
Alan Louis <alouis811@gmail.com>
2023-06-01 18:29:16 +0530

    zz

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
-let myLib = [];
+let myLib = [{title:"The Hobbit", author:"J.R.R Tolkein",pages:295,status:"Read"}];
 let count = 0;
 let cont = document.getElementsByClassName("container");
 let btn = document.getElementById("AddBooks")
 let over = document.getElementById("overlay");
 let formCont = document.getElementById("formCont")
+let Form = document.querySelector(".subForm")
 
 console.log(btn);
 
 btn.addEventListener("click", () =>{
     over.classList.add("active")
     formCont.classList.add("active")
+})
+
+// I'm using "click" but it works with any event
+document.addEventListener('click', event => {
+    const isClickInside = formCont.contains(event.target) || btn.contains(event.target)
+    if (!isClickInside) {
+      over.classList.remove("active")
+      formCont.classList.remove("active")
+      let m = document.getElementById("Title").value;
+    }
 })
 
 function book(title, author, pages, status) {
@@ -20,13 +31,10 @@ function book(title, author, pages, status) {
     this.info = function () { return (title + " by " + author + ", " + pages + " pages, " + status); }
 }
 
-const a = new book("The Hobbit", "J.R.R. Tolkein", "295", "not read yet ");
-const b = new book("Peanuts", "Charles Schulz", "75", "read");
-
 function pushDocument(){
     cont[0].innerHTML = ""
     myLib.forEach((i) => {
-        cont[0].innerHTML += `<div> <p>Title: ${i.title} </p> <p>Author: ${i.author}</p> <p>Pages: ${i.pages}</p> <p>Status: ${i.status}</p> </div>`
+        cont[0].innerHTML += `<div> <span class="remove-book"> X </span> <p>Title: "${i.title}" </p> <p>Author: "${i.author}"</p> <p>Pages: ${i.pages}</p> <p>Status: ${i.status}</p> </div>`
     }
     )
 }
@@ -35,16 +43,18 @@ function addBookToLibrary(newbook) {
     myLib.push(newbook);
     pushDocument()
 }
-addBookToLibrary(b)
+// addBookToLibrary(b)
 
-// I'm using "click" but it works with any event
-document.addEventListener('click', event => {
-  const isClickInside = formCont.contains(event.target) || btn.contains(event.target)
-  if (!isClickInside) {
-    over.classList.remove("active")
-    formCont.classList.remove("active")
-    let m = document.getElementById("Title").value;
-    console.log(m)
-    // The click was OUTSIDE the specifiedElement, do something
-  }
-})
+Form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("HEllo");
+    let tit = document.getElementById("Title").value;
+    let aut = document.getElementById("Author").value;
+    let pag = (document.getElementById("Pages").value);
+    let read = document.getElementById("Read").checked?"Read":"Not read yet";
+    let nb = new book(tit,aut,pag,read);
+    addBookToLibrary(nb);
+    Form.reset();
+  });
+
+pushDocument()

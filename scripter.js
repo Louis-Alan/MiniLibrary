583c15d2620141f2e5f0b5c93b0df49c717e6861
Alan Louis <alouis811@gmail.com>
2023-06-18 14:00:02 +0530

    connecting to api

---
+++ scripter.js
@@ -0,0 +1,21 @@
+let cont = document.getElementsByClassName("container");
+
+function displayBooks() {
+    fetch('http://localhost:8080/api/v1/Book')
+        .then(response => response.json())
+        .then(data => {
+            const books = data; // Assuming the response is assigned to a variable named 'data'
+
+            // Iterate through each book and create HTML elements
+            books.forEach(i => {
+
+                cont[0].innerHTML += `<div> <span class="remove-book"> X </span> <p>Title: "${i.title}" </p> <p>Author: "${i.author}"</p> <p>Pages: ${i.pages}</p> <p>Status: ${i.status}</p> </div>`
+
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+displayBooks();
